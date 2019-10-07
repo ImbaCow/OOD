579b343d6c7c49d00ce114de4ce01c50a82cd88d
Егор <e.iambakov@gmail.com>
2019-10-07 11:56:35 +0300

    lab04 Добавил обработку исключения

--- lab04/fabric/Painter/main.cpp
+++ lab04/fabric/Painter/main.cpp
@@ -9,11 +9,18 @@ using namespace std;
 
 int main()
 {
-	shared_ptr<IShapeFactory> factory = make_shared<CShapeFactory>();
-	CDesigner designer(factory);
-	CPainter painter;
+	try
+	{
+		shared_ptr<IShapeFactory> factory = make_shared<CShapeFactory>();
+		CDesigner designer(factory);
+		CPainter painter;
 
-	CPictureDraft draft = designer.CreateDraft(cin);
-	shared_ptr<ICanvas> canvas = make_shared<CSvgCanvas>("test.svg", 100, 100, 2);
-	painter.DrawPicture(draft, canvas);
+		CPictureDraft draft = designer.CreateDraft(cin);
+		shared_ptr<ICanvas> canvas = make_shared<CSvgCanvas>("test.svg", 100, 100, 2);
+		painter.DrawPicture(draft, canvas);
+	}
+	catch (const std::exception& ex)
+	{
+		cerr << ex.what() << endl;
+	}
 }
